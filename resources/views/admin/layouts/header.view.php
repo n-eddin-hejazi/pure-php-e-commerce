cf9a6ba54d29c9b6e0b5fbd74fb07e019e14e245
Nour ALJELANI <89624096+nouraljelani@users.noreply.github.com>
2023-03-11 00:36:10 +0300

    edit navbar

--- resources/views/admin/layouts/header.view.php
+++ resources/views/admin/layouts/header.view.php
@@ -10,8 +10,8 @@
 <body>
      <?php include view_path() . 'admin/layouts/navbar.view.php'; ?>   
      <?php include view_path() . 'admin/layouts/sidebar.view.php'; ?>
-     <div class="p-4 sm:ml-64 ">
-          <div class="p-4  border border-1 border-gray-200 dark:border-gray-700 mt-14">
+     <div class="p-4 sm:ml-64">
+          <div class="p-4 mt-14">
           <?php include view_path() . 'admin/layouts/success-message.view.php'; ?>   
           <?php include view_path() . 'admin/layouts/fail-message.view.php'; ?>   
 
