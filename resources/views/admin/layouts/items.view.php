cb816db6ccee7a15b713d452128116745ccd7271
Nour ALJELANI <89624096+nouraljelani@users.noreply.github.com>
2023-03-11 02:43:29 +0300

    add users item in sidebar

--- resources/views/admin/layouts/items.view.php
+++ resources/views/admin/layouts/items.view.php
@@ -5,6 +5,12 @@
     </a>
 </li>
 
+<li>
+    <a href="<?= main_url() ?>/admin/users" class="text-sm flex items-center p-2 font-normal text-gray-900 rounded-md dark:text-white hover:bg-gray-100 dark:hover:bg-gray-700">
+        <svg xmlns="http://www.w3.org/2000/svg" class="w-5 h-5 text-gray-500 transition duration-75 dark:text-gray-400 group-hover:text-gray-900 dark:group-hover:text-white" width="16" height="16" fill="currentColor" class="bi bi-people-fill" viewBox="0 0 16 16"><path d="M7 14s-1 0-1-1 1-4 5-4 5 3 5 4-1 1-1 1H7Zm4-6a3 3 0 1 0 0-6 3 3 0 0 0 0 6Zm-5.784 6A2.238 2.238 0 0 1 5 13c0-1.355.68-2.75 1.936-3.72A6.325 6.325 0 0 0 5 9c-4 0-5 3-5 4s1 1 1 1h4.216ZM4.5 8a2.5 2.5 0 1 0 0-5 2.5 2.5 0 0 0 0 5Z"/></svg>
+        <span class="ml-3">Users</span>
+    </a>
+</li>
 
 
 <!-- <li>
